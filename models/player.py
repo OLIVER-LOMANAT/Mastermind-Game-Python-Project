4313a284ac5392f726a1e583f3e9bf454c65aa9f
Oliver Lomanat <lomanatoliver@gmail.com>
2025-08-26 12:09:28 +0300

    defining the create player method and get all players and find player

--- models/player.py
+++ models/player.py
@@ -15,3 +15,32 @@ class Player:
         CONN.commit()
         self.id = CURSOR.lastrowid
 
+def create_player_table():
+    sql = """
+        CREATE TABLE IF NOT EXISTS players (
+        id INTEGER PRIMARY KEY,
+        username TEXT UNIQUE
+        )
+    """
+    CURSOR.execute(sql)
+    CONN.commit()
+
+def get_all_players():
+    sql = "SELECT * FROM players"
+    CURSOR.execute(sql)
+    rows = CURSOR.fetchall()
+    players = []
+    for row in rows:
+        player = Player(row[1], row[0])
+        players.append(player)
+    return players
+
+
+def find_player_by_username(username):
+    sql = "SELECT * FROM players WHERE username = ?"
+    CURSOR.execute(sql, (username,))
+    row = CURSOR.fetchone()
+    if row:
+        return Player(row[1], row[0])
+    return None
+
