3a76407c1cf45b9955a7b1e4eefd981afc7ca981
Oliver Lomanat <lomanatoliver@gmail.com>
2025-08-26 12:17:11 +0300

    Defining the initialize function to call the create methods

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import random
+from models.player import create_player_table, find_player_by_username, get_all_players, Player
+from models.game import get_games_by_player, create_game_table, Game
+
+def initialize_database():
+    create_player_table()
+    create_game_table()
+
